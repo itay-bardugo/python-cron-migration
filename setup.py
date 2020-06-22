398df643713d5d54fe0e060abc2c6dabaf5a3b2a
itay-bardugo <itaybardugo91@gmail.com>
2020-06-22 08:52:38 +0300

    bug fix: add isntall_requires flag to install dependecies

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+main_path = os.path.dirname(os.path.realpath(__file__))
+requirements_file = main_path + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirements_file):
+    with open(requirements_file) as f:
+        while (line := f.readline()):
+            if not line.startswith("-"):
+                install_requires.append(line.strip())
+
 setuptools.setup(
     name="itay-bardugo-cron-migration", # Replace with your own username
-    version="1.1.0",
+    version="1.1.1",
     author="Itay Bardugo",
     author_email="itaybardugo91@gmail.com",
     description="manage your cron jobs with python and a revision system(CLI)",
@@ -13,6 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/itay-bardugo/python-cron-migration",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
